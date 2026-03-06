02b91c218e9778b2fa5a4dd47ee0d3068ad66b09
Sahil <sahilnagel@gmail.com>
2026-03-06 10:19:12 +0530

    Initial commit: Setup Express server and environment variables

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/api/health', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'Welcome to the server , Its healthy'
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is up and running on https://localhost:${PORT}`);
+});
+
